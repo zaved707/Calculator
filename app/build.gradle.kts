e8a31f3955bf417a60288bff26addead50138617
Zaved <zavedahmadca707@gmail.com>
2025-06-10 16:48:06 +0530

    version code update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 1
-        versionName = "0.2"
+        versionName = "0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
